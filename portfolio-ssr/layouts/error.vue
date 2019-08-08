f22b9753feb7cc7c793b3bd627224fa80524bd5e
amaliauka <aleksmalyavko@gmail.com>
2019-08-08 17:58:08 +0300

    created simple example

---
+++ portfolio-ssr/layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    Page not found
+  </div>
+</template>
+
+<style>
+</style>
+
---
+++ portfolio-ssr/pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    About Page
+    <nuxt-link to="/">Main Page</nuxt-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    // AppLogo
+  }
+}
+</script>
+
+<style>
+</style>
