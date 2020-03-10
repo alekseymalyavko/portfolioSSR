2fae5309ed19d0ee6211aee4d0c9c25b0ac60ff3
aliaksei-maliauka <aliaksei.maliauka@hp.com>
2020-03-10 18:39:12 +0300

    updated

--- portfolio-ssr/pages/about/index.vue
+++ portfolio-ssr/pages/about/index.vue
@@ -1,45 +1,30 @@
 <template>
   <div>
-    About Page
-    <p> {{ title }} </p>
+    <p class="lol" v-on:click="lol"> {{ title }} </p>
     <nuxt-link to="/">Main Page</nuxt-link>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 export default {
-  components: {
-    // AppLogo
-  },
-  data () {
-    console.log(1)
+  data() {
     return {
-      title: 'Hello World!'
+      title: 'About Page'
     }
   },
   head() {
     return {
-      title: this.title
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'About description' }
+      ]
     }
   },
-  // head () {
-  //   return {
-  //     title: this.title,
-  //     meta: [
-  //       // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-  //       { hid: 'description', name: 'description', content: 'My custom description' }
-  //     ]
-  //   }
-  // },
-  asyncData (context) {
-    console.log(2)
-    // called every time before loading the component
-    // as the name said, it can be async
-    // Also, the returned object will be merged with your data object
-    axios.get('lol.com')
-    return { title: 'World' }
-  },
+  methods: {
+    lol() {
+      console.log(123)
+    }
+  }
 }
 </script>
 
