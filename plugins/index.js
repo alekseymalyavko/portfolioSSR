344bb33e9bc3b1d22da9fa1c78833de354702bec
aliaksei-maliauka <aliaksei.maliauka@hp.com>
2020-05-15 19:35:48 +0300

    updated

--- plugins/index.js
+++ plugins/index.js
@@ -2,9 +2,5 @@ import Vue from "vue"
 import VueWaypoint from "vue-waypoint"
 import VueAgile from 'vue-agile'
 
-VueWaypoint.animate = function(e) {
-  console.log(23213, e)
-}
-console.log(VueWaypoint)
 Vue.use(VueWaypoint)
 Vue.use(VueAgile)
